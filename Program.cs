5c26e2f494a41eb0a97073f97bf5b37ce3e2ce8c
bruwyvn <bruwyvn@outlook.com>
2022-10-10 21:31:21 -0300

    Wipe migrations (#4)
    * Adiciona modelo `Emprestimo`
    
    * WIP: Empréstimo em andamento
    
    * WIP: Empréstimo, alterações do Lucas
    
    * WIP: Alterações novas
    
    * Corrige erro de sintaxe
    
    * WIP: Alterações de usuário
    
    * Wipe migrations
    
    Co-authored-by: Bruno Andrade <bruno.andrade@sambay.com.br>

--- Program.cs
+++ Program.cs
@@ -14,11 +14,7 @@
 	dotnet ef migrations add InitialCreate
 	dotnet ef database update
 */
-using System;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace Biblioteca;
 
@@ -28,24 +24,33 @@ class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        var connectionString = builder.Configuration.GetConnectionString("Biblioteca") ?? "Data Source=Biblioteca.db";
+        var connectionString = builder.Configuration.GetConnectionString("biblioteca") ?? "Data Source=biblioteca.db";
         builder.Services.AddSqlite<BibliotecaContext>(connectionString);
         var app = builder.Build();
 
 
+        // LIVRO
         Livro livro = new Livro();
-        app.MapGet("/livros", livro.Listar);
-        app.MapGet("/livros/{id}", livro.ListarId);
-        app.MapPost("/cadastrar/livros", livro.Cadastrar);
-        app.MapPut("/atualizar/livros/{id}", livro.Atualizar);
-        app.MapDelete("/deletar/livros/{id}", livro.Deletar);
+        app.MapGet("/livro", livro.Listar);
+        app.MapGet("/livro/{id}", livro.ListarId);
+        app.MapPut("/livro", livro.Cadastrar);
+        app.MapDelete("/livro/{id}", livro.Deletar);
+        app.MapPut("/atualizar/book/{id}", livro.Atualizar);
 
-        // USUÁRIO
-        Usuario newUser = new Usuario();
-        app.MapGet("/user", newUser.Listar);
-        app.MapPost("/cadastrar/user", newUser.Cadastrar);
-        app.MapGet("/deletar/user/{id}", newUser.Deletar);
-        app.MapPost("/atualizar/user/{id}", newUser.Atualizar);
+        // Usuário
+        Usuario usuario = new Usuario();
+        app.MapGet("/usuario", usuario.Listar);
+        app.MapGet("/usuario/{id}", usuario.Buscar);
+        app.MapPost("/usuario", usuario.Cadastrar);
+        app.MapPut("/usuario", usuario.Atualizar);
+        app.MapDelete("/usuario", usuario.Deletar);
+
+        // Empréstimo
+        Emprestimo emprestimo = new Emprestimo();
+        app.MapGet("/emprestimo", emprestimo.Listar);
+        app.MapGet("/emprestimo/{id}", emprestimo.Buscar);
+        app.MapPost("/emprestimo", emprestimo.Cadastrar);
+        app.MapPut("/emprestimo", emprestimo.Atualizar);
 
         app.Run();
     }
