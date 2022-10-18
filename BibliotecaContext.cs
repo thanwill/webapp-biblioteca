ff931eb3cd49ee07de6acc5a89c10f6b9e65ae4a
Jonathan Pereira <jonathan14willian@icloud.com>
2022-10-18 10:30:06 -0300

    Update Final (#6)
    * Wipe migrations (#4)
    
    * Adiciona modelo `Emprestimo`
    
    * WIP: Empréstimo em andamento
    
    * WIP: Empréstimo, alterações do Lucas
    
    * WIP: Alterações novas
    
    * Corrige erro de sintaxe
    
    * WIP: Alterações de usuário
    
    * Wipe migrations
    
    Co-authored-by: Bruno Andrade <bruno.andrade@sambay.com.br>
    
    * Correções
    
    Co-authored-by: bruwyvn <bruwyvn@outlook.com>
    Co-authored-by: Bruno Andrade <bruno.andrade@sambay.com.br>

--- BibliotecaContext.cs
+++ BibliotecaContext.cs
@@ -1,18 +1,14 @@
-﻿using System;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace Biblioteca;
 
 // Uma instância DbContext representa uma sessão com o banco de dados e pode ser usada para consultar e salvar instâncias de suas entidades
 // Veja os métodos: https://learn.microsoft.com/en-us/dotnet/api/microsoft.entityframeworkcore.dbcontext?view=efcore-6.0
-public class BibliotecaContext: DbContext
+public class BibliotecaContext : DbContext
 {
 
     // DbContextOptions: This is an internal API that supports the Entity Framework Core infrastructure
-    public BibliotecaContext(DbContextOptions options) : base(options){}
+    public BibliotecaContext(DbContextOptions options) : base(options) { }
 
     //Se as propriedades DbSet<TEntity> tiverem um setter público, elas serão inicializadas automaticamente quando a instância do contexto derivado for criada.
 
@@ -21,6 +17,10 @@ public class BibliotecaContext: DbContext
 
     // Cria a tabela Usuarios
     public DbSet<Usuario> Usuarios { get; set; } = null!;
+
+    // Cria a tabela Emprestimos
+
+    public DbSet<Emprestimo> Emprestimos { get; set; }
 
     // IMPORTANTES MÉTODOS
 
--- Usuario.cs
+++ Usuario.cs
@@ -1,55 +1,107 @@
-﻿using System;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-
+﻿using Newtonsoft.Json;
 namespace Biblioteca;
 
 public class Usuario
 {
     public int Id { get; set; }
-    public string? Nome { get; set; }
-    public string? CPF { get; set; }
-    public string? Email { get; set; }
-    public string? Telefone { get; set; }
+    public string Nome { get; set; }
+    public string CPF { get; set; }
+    public string Email { get; set; }
+    public string Telefone { get; set; }
 
-    public List<Usuario> listarUsuarios(BibliotecaContext banco)
+    public string Listar(BibliotecaContext banco)
     {
-        return banco.Usuarios.ToList();
+        return JsonConvert.SerializeObject(banco.Usuarios.ToList(), Formatting.Indented);
     }
 
-    public Usuario Listar(BibliotecaContext banco, int id)
+    public string Buscar(BibliotecaContext banco, int id)
     {
-        return banco.Usuarios.Find(id);
+        return JsonConvert.SerializeObject(banco.Usuarios.Find(id));
     }
 
-    public string Cadastrar(BibliotecaContext banco, Usuario usuario)
+    public int Cadastrar(BibliotecaContext banco, Usuario usuario)
     {
-        int cont = 0;
-
-        usuario.Id = cont++;
-        banco.Usuarios.Add(usuario);
-        banco.SaveChanges();
-        return "Usuário cadastrado com sucesso";
+        bool cadExistente = Validar(banco, usuario);
+        if (cadExistente == false)
+        {
+            banco.Usuarios.Add(usuario);
+        }
+        else
+        {
+            throw new Exception("Este usuário já foi cadastrado anteriormente");
+        }
+        return banco.SaveChanges();
     }
 
-    public string Atualizar(BibliotecaContext banco, Usuario atualizado, int id)
+    public int Atualizar(BibliotecaContext banco, Usuario atualizado, int id)
+    {
+        bool mailExistente = ValidarAtt(banco, atualizado, id);
+
+        if (mailExistente == false)
+        {
+            var usuario = banco.Usuarios.Find(id);
+            usuario.Nome = atualizado.Nome;
+            usuario.Email = atualizado.Email;
+            usuario.Telefone = atualizado.Telefone;
+            return banco.SaveChanges();
+        }
+        else
+        {
+            throw new Exception("As alterações não foram concluidas pois já existe um email com estas informações");
+        }
+    }
+
+    public int Deletar(BibliotecaContext banco, int id)
     {
         var usuario = banco.Usuarios.Find(id);
-        usuario.Nome = atualizado.Nome;
-        usuario.Email = atualizado.Email;
-        usuario.Telefone = atualizado.Telefone;
-
-        banco.SaveChanges();
-        return "Usuário atualizado com sucesso";
+        if (usuario != null)
+        {
+            banco.Remove(usuario);
+            return banco.SaveChanges();
+        }
+        else
+        {
+            throw new Exception("Talvez você esteja tentando deletar um usuário que não existe");
+        }
     }
 
-    public string Deletar(BibliotecaContext banco, int id)
+    public bool Validar(BibliotecaContext banco, Usuario usuario)
     {
-        var usuario = banco.Usuarios.Find(id);
-        banco.Remove(usuario);
-        banco.SaveChanges();
-        return "Usuario deletado";
+        bool cadExistente = false;
+        foreach (var busca in banco.Usuarios)
+        {
+            if (busca.CPF == usuario.CPF || busca.Email == usuario.Email)
+            {
+                cadExistente = true;
+                break;
+            }
+        }
+
+        return cadExistente;
     }
+
+    public bool ValidarAtt(BibliotecaContext banco, Usuario usuario, int id)
+    {
+        var user = banco.Usuarios.Find(id);
+        string mail = user.Email;
+        user.Email = "";
+        bool cadExistente = false;
+
+        foreach (var busca in banco.Usuarios)
+        {
+            if (busca.Email == usuario.Email)
+            {
+                user.Email = mail;
+                cadExistente = true;
+                break;
+            }
+        }
+
+        return cadExistente;
+    }
+}
+
+public class ListarId
+{
+    public int Id { get; set; }
 }
