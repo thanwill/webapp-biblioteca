df23ea71a352e8859af7dd48d851a71772677769
Jonathan Pereira <jonathan14willian@icloud.com>
2022-11-16 01:45:17 -0300

    Função excluir usuário
    - ajuste da rota na API, id não era passado como parâmetro.
    - o modal de visualização passou a ser carregado dinamicamente.
    - link de excluir passou a receber a função excluir_usuario(id)

--- View/assets/js/usuario.js
+++ View/assets/js/usuario.js
@@ -56,8 +56,8 @@ function lista_usuarios() {
         .then((response) => response.json())
         .then((usuarios) => {
             for (const {
-                    Id, 
-                    Nome, 
+                    Id,
+                    Nome,
                     Sobrenome
                 } of usuarios) {
                 const itemLista = document.createElement("li");
@@ -87,31 +87,77 @@ function visualizar_perfil(id) {
         .then((response) => response.json())
         .then((usuario) => {
             const visualizaPerfil = document.querySelector('#visualizar-perfil');
-            let list = 
-            `
-            <tbody>
-            <tr>
-              <td>Nome completo</td>
-              <td>${usuario.Nome} ${usuario.Sobrenome}</td>
-            </tr>
-            <tr>
-              <td>CPF</td>
-              <td>${usuario.CPF}</td>
-            </tr>
-            <tr>
-              <td>E-mail</td>
-              <td>${usuario.Email}</td>
-            </tr>
-            <tr>
-              <td>Telefone</td>
-              <td>${usuario.Telefone}</td>
-            </tr>
-            <tr>
-              <td>Nascimento</td>
-              <td>${usuario.Nascimento}</td>
-            </tr>
-          </tbody>
+            let list =
+                `
+            <div class="modal-content">
+            <div class="row" style="padding-top: 1vh">
+              <h4 class="grey-text left-align">Usuário</h4>
+              <p class="grey-text left-align">
+                Consulte as informações cadastradas do usuário selecionado.
+              </p>
+            </div>
+            <div class="row">
+              <table class="grey-text striped">
+              <tbody>
+              <tr>
+                <td>Matrícula</td>
+                <td>${usuario.Id}</td>
+              </tr>
+              <tr>
+                <td>Nome completo</td>
+                <td>${usuario.Nome} ${usuario.Sobrenome}</td>
+              </tr>
+              <tr>
+                <td>CPF</td>
+                <td>${usuario.CPF}</td>
+              </tr>
+              <tr>
+                <td>E-mail</td>
+                <td>${usuario.Email}</td>
+              </tr>
+              <tr>
+                <td>Telefone</td>
+                <td>${usuario.Telefone}</td>
+              </tr>
+              <tr>
+                <td>Nascimento</td>
+                <td>${usuario.Nascimento}</td>
+              </tr>
+            </tbody>
+              </table>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <a href="#formulario-usuario" class="modal-close waves-effect waves-green btn-flat">Editar</a>
+            <a class="modal-close waves-effect waves-green btn-flat" onclick(excluir_usuario(${usuario.Id}))>Excluir</a>
+          </div>
+            
             `;
-            visualizaPerfil.innerHTML = list;            
+            visualizaPerfil.innerHTML = list;
         });
+}
+
+function excluir_usuario(id) {
+    fetch(url + 'usuarios/' + id, {
+            'method': 'DELETE',
+            'redirect': 'follow'
+        })
+        .then((response) => {
+            if (response.ok) {
+                return response.text()
+            } else {
+                return response.text().then((text) => {
+                    throw new Error(text)
+                })
+            }
+        })
+        .then((output) => {
+            listar()
+            console.log(output)
+            alert('Usuário removido! >=]')
+        })
+        .catch((error) => {
+            console.log(error)
+            alert('Não foi possível remover o usuário :/')
+        })
 }
